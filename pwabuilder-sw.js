9f6b6e7af93532d6d2a94d9ce8d443f8f07bee42
harddog1 <harddog12@naver.com>
2025-07-10 02:55:49 +0900

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -19,7 +19,7 @@ var contentToCache = offlineFallbackPage;
 self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(CACHE).then(function (cache) {
       console.log("[Service Worker] Caching all: app shell and content");
       return cache.addAll(contentToCache);
     }),
@@ -33,7 +33,7 @@ self.addEventListener("fetch", function (e) {
       return (
         r ||
         fetch(e.request).then(function (response) {
-          return caches.open(cacheName).then(function (cache) {
+          return caches.open(CACHE).then(function (cache) {
             console.log(
               "[Service Worker] Caching new resource: " + e.request.url,
             );
