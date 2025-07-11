3a10ac387ec0c257538946951b7670dd0b385bae
harddog1 <harddog12@naver.com>
2025-07-12 03:06:10 +0900

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'my-cache-v1';
-const FILES_TO_CACHE = [
-  "index.html",
-  "a.html",
-  "b.html",
-  "index.css",
-  "index.js",
-  "icons/icon-192.png",
+let cachename = "hello";
+let cachefiles = [
+    "index.html",
+    "a.html",
+    "b.html",
+    "index.css",
+    "index.js",
+    "icons/icon-192.png",
 ];
 
 self.addEventListener('install', (event) => {
